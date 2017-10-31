cc0e1b11caef024f57a0f9afb342c42d20ae5d79
luchk <30504944+luchk@users.noreply.github.com>
2017-10-31 23:24:15 +0200

    Шоста лаба до Злобіна (Здана)

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main() {
+    float x, y;
+    FILE *myfile;
+    myfile = fopen("/ProgrammingLab6/myfile.txt", "wt");
+    x = 0;
+
+    while (x <= 5) {
+        y = (pow(x,2)) / 4 + x - 1.2502;
+
+        fprintf(myfile,"%f   %f \n", x,y);
+
+        x += 0.000002;
+    }
+    return 0;
+}
